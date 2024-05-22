42e8362ada94cd9269cc0721fbdb4eebd1109f14
EDITH PARINANGO <edyparinango@gmail.com>
2024-05-22 09:05:35 -0500

    trabajo8

--- ejercicio_8.py
+++ ejercicio_8.py
@@ -1,2 +1,15 @@
 #Escribir un programa que pida al usuario una palabra y luego muestre por pantalla
 # la primera letra la letra que se encuentra en medio y la ultima letra separadas por comas(,).
+
+palabra = input("Introduce una palabra: ")
+
+longitud = len(palabra)
+primera_letra = palabra[0]
+ultima_letra = palabra[-1]
+
+if longitud % 2 == 0:
+    letra_media = palabra[longitud // 2 - 1]
+else:
+    letra_media = palabra[longitud // 2]
+
+print("Las letras extremas de la palabra '{}' son: {}, {}, {}".format(palabra, primera_letra, letra_media, ultima_letra))
--- ejercicio_9.py
+++ ejercicio_9.py
@@ -1,2 +1,11 @@
 #Escriba un programa que pregunte cuántos números se van a introducir, luego pida esos números, 
 # y muestre un mensaje cada vez que un número no sea mayor que el anterior.
+
+cantidad_numeros = int(input("¿Cuántos números vas a introducir? "))
+anterior = float(input("Introduce el primer número: "))
+
+for i in range(1, cantidad_numeros):
+    numero = float(input("Introduce el siguiente número: "))
+    if numero <= anterior:
+        print("El número introducido no es mayor que el anterior.")
+    anterior = numero
